afa1f1c3aaf6b220aeae34233046ded878a59d74
Kevin Ricardino <kevin.j.bernardes@gmail.com>
2026-01-28 08:49:45 -0300

    Initial project structure

---
+++ crawler.py
@@ -0,0 +1,16 @@
+import requests
+
+URL_AUTOMOVEIS = "https://django-anuncios.solyd.com.br/automoveis/"
+
+def buscar(url):
+    try:
+        resposta = requests.get(url)
+        if resposta.status_code == 200:
+            print(resposta.text)
+        else:
+            print("> Erro ao fazer requisição!")
+    except Exception as error:
+        print("> Erro ao fazer requisição!")
+        print(error)
+
+buscar(URL_AUTOMOVEIS)
